fd1366f20b6d08bbbe399ab0a7591b5aa848233c
SH Baek <seunghyebaek0509@gmail.com>
2023-05-17 11:43:17 +0900

    updated

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import "./about.css";
 import AboutImg from "../../assets/about.jpg";
-import CV from "../../assets/John-Cv.pdf";
+import CV from "../../assets/SeunghyeBaek_Resume.pdf";
 import Info from './Info';
 
 
